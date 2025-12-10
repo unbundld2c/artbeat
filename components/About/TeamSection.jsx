e01d1bc9c5fbc4f6dcc45c33321d326866f6c0c2
saumyaunb007 <saumyasrivastava@unbundl.com>
2025-12-10 18:29:32 +0530

    add linkedin on aboutus page

--- components/About/TeamSection.jsx
+++ components/About/TeamSection.jsx
@@ -6,21 +6,25 @@ const teamMembers = [
     name: "Supriya Iyengar",
     role: "Founding Member",
     imgSrc: assets.TeamMember1,
+    linkedin: "https://www.linkedin.com/in/supriyaiyengar/",
   },
   {
     name: "Ekta Sethi",
     role: "Founder",
     imgSrc: assets.TeamMember2,
+    linkedin: "https://www.linkedin.com/in/gurekta-sethi-944373b7/",
   },
   {
     name: "Pallavi J. Goswami",
     role: "Founding Member",
     imgSrc: assets.TeamMember3,
+    linkedin: "https://www.linkedin.com/in/pallavi-goswami-54231517/",
   },
   {
     name: "Charu Chadha",
     role: "Founding Member",
     imgSrc: assets.TeamMember4,
+    linkedin: "https://www.linkedin.com/in/charu-chadha-7a47b74/",
   },
 ];
 
@@ -41,7 +45,7 @@ const TeamSection = () => {
         {teamMembers.map((member, index) => (
           <div
             key={index}
-            className="teamMember flex flex-col items-center rounded-lg "
+            className="teamMember flex flex-col items-center rounded-lg gap-1"
           >
             <div
               className={` mb-[8px] rounded-full flex items-center justify-center`}
@@ -54,9 +58,24 @@ const TeamSection = () => {
                 height="auto"
                 className="rounded-full object-cover"
               />
-            </div>
-            <h3 className="font-bold text-[16px] md:text-[20px] leading-[20px]  md:leading-[26px] text-[#2C2C2C] mb-[4px[">{member.name}</h3>
+            </div>  
+            <h3 className="font-bold text-[16px] md:text-[20px] leading-[20px]  md:leading-[26px] text-[#2C2C2C]">{member.name}</h3>
             <p className="text-[16px] leading-[24px] text-[#2C2C2C] font-normal">{member.role}</p>
+            
+            <a
+              href={member.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 font-semibold text-[#0077B5]"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M7 17V10M21 8V16C21 17.3261 20.4732 18.5979 19.5355 19.5355C18.5979 20.4732 17.3261 21 16 21H8C6.67392 21 5.40215 20.4732 4.46447 19.5355C3.52678 18.5979 3 17.3261 3 16V8C3 6.67392 3.52678 5.40215 4.46447 4.46447C5.40215 3.52678 6.67392 3 8 3H16C17.3261 3 18.5979 3.52678 19.5355 4.46447C20.4732 5.40215 21 6.67392 21 8Z" stroke="#0077B5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11 17V13.75M11 13.75V10M11 13.75C11 10 17 10 17 13.75V17M7 7.01002L7.01 6.99902" stroke="#0077B5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+              <span className="text-[14px] md:text-[16px] font-semibold leading-[150%] text-[#0077B5] rounded hover:underline">
+                View Profile
+              </span>
+            </a>
           </div>
         ))}
       </div>
